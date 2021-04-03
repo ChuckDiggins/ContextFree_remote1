1b619c70962f26e1ccf35b3efe1ac39372fa7c20
Chuck Diggins <cfdiggins@yahoo.com>
2021-04-03 09:24:24 -0600

    Initial Commit

---
+++ ContextFree/ContextFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextFreeApp.swift
+//  ContextFree
+//
+//  Created by Charles Diggins on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ContextFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
